ff688c2d5720676a5131e3ff386870bb4e1335b0
Axitsavaliya <168092847+Axitsavaliya@users.noreply.github.com>
2024-06-11 14:29:57 +0530

    Update lastadd.js

--- lastadd.js
+++ lastadd.js
@@ -1,4 +1,8 @@
 let arr=[2,5,1,3,5,6,7];
 let num=10;
-arr[arr.length] =num;
-console.log(arr);
+// arr[arr.length] =num;
+// console.log(arr);
+while (arr[arr.length-1]<num) {
+    arr[arr.length]=num
+    console.log(arr);
+}
